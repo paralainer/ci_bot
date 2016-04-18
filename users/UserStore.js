2287e65c70ab58f3de450d43dfdabdc29416418b
paralainer <serg.talov@gmail.com>
2016-04-18 16:30:00 +0300

    Fixed duplicate issue

--- users/UserStore.js
+++ users/UserStore.js
@@ -2,12 +2,14 @@ var UserStore = function (db) {
     this.db = db;
 };
 
-UserStore.prototype.save = function(prefix, credentials, callback) {
+UserStore.prototype.save = function (prefix, credentials, callback) {
     var collection = this.db.collection(prefix + '_users');
-    collection.insertOne(credentials).then(callback);
+    collection.findOneAndDelete({id: credentials.id}).then(function () {
+        collection.insertOne(credentials).then(callback)
+    });
 };
 
-UserStore.prototype.find = function(prefix, id, callback) {
+UserStore.prototype.find = function (prefix, id, callback) {
     var collection = this.db.collection(prefix + '_users');
     collection.findOne({id: id}).then(callback);
 };
@@ -15,9 +17,9 @@ UserStore.prototype.find = function(prefix, id, callback) {
 var userStore = null;
 
 module.exports.init = function (db) {
-   userStore = new UserStore(db);
+    userStore = new UserStore(db);
 };
 
 module.exports.get = function () {
-  return userStore;
+    return userStore;
 };
